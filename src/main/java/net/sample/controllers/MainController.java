eb02e6a7017ad9c6999eaa38704e1f4b160ffffe
Ukon1990 <mail@jonaskf.net>
2019-05-23 21:21:22 +0200

    Can find a new file name for a non season and episode based file name

--- src/main/java/net/sample/controllers/MainController.java
+++ src/main/java/net/sample/controllers/MainController.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import net.sample.models.Episode;
 import net.sample.models.FileToReplace;
 import net.sample.models.Show;
 import net.sample.utils.TVDBUtil;
@@ -14,11 +15,14 @@ import java.io.File;
 import java.net.URL;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
 
 import static net.sample.utils.FileReader.openFolderPicker;
+import static net.sample.utils.Regex.getMatches;
 
 public class MainController implements Initializable {
-    TVDBUtil tvdb = new TVDBUtil();
+    private TVDBUtil tvdb = new TVDBUtil();
+    private Show show;
 
     @FXML
     private Parent episodeTable;
@@ -49,6 +53,7 @@ public class MainController implements Initializable {
             episodeLabel.setText(show.getEpisodes().size() + "");
             show.getEpisodes()
                     .forEach(episodeTableController::add);
+            this.show = show;
         }
     }
 
@@ -63,19 +68,54 @@ public class MainController implements Initializable {
 
         folderPathField.setText(s);
 
-        if (files != null) {
+        if (files != null && this.show != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    fileTableController.add(
-                            new FileToReplace(file.getName(),
-                                    "TODO",
-                                    "Season x"));
+                    Matcher numberMatch = getMatches(
+                            file.getName(),
+                            "([\\d]{1,})");
+                    if (numberMatch.find()) {
+                        int index = Integer.parseInt(numberMatch.group(0));
+                        addFileIfEpisodeMatchFound(file, index);
+                    } else {
+                        System.out.println("Number not found: " + file.getName());
+                    }
                 }
             }
         }
 
 
         return true;
+    }
+
+    private void addFileIfEpisodeMatchFound(File file, int index) {
+        if (this.show.getEpisodes().get(index) != null) {
+            Episode episode = this.show.getEpisodes().get(index);
+            Matcher extensionMatch = getMatches(
+                    file.getName(),
+                    "(\\.[a-z]{1,5})");
+
+            if (extensionMatch.find()) {
+                fileTableController.add(
+                        new FileToReplace(
+                                index,
+                                file.getName(),
+                                getNewName(episode, extensionMatch),
+                                "Season " + episode.getSeason(),
+                                file));
+            }
+        }
+    }
+
+    private String getNewName(Episode episode, Matcher extensionMatch) {
+        return nameLabel.getText() +
+                " - s" + episode.getSeason() +
+                "e" + getEpisodeString(episode) +
+            extensionMatch.group(0);
+    }
+
+    private Object getEpisodeString(Episode episode) {
+        return episode.getEpisode() < 10 ? "0" +  episode.getEpisode() :  episode.getEpisode();
     }
 
     private void setEpisodeColumns() {
@@ -87,16 +127,21 @@ public class MainController implements Initializable {
     }
 
     private void setFileColumns() {
+        fileTableController.addColumn("Number", "number", Integer.class);
         fileTableController.addColumn("Name", "currentName", String.class);
+        fileTableController.addColumn("Folder", "seasonFolder", String.class);
         fileTableController.addColumn("New name", "newName", String.class);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         searchField.setText("One piece");
+        folderPathField.setText("/Users/jonas/Dropbox/scripts_unix/scripts/one-piece-renamer/input");
 
         setEpisodeColumns();
         setFileColumns();
+        this.search(null);
+        this.handleDirectorySelection(folderPathField.getText());
     }
 
 }
--- src/main/java/net/sample/models/FileToReplace.java
+++ src/main/java/net/sample/models/FileToReplace.java
@@ -1,14 +1,36 @@
 package net.sample.models;
 
+import java.io.File;
+
 public class FileToReplace {
+    private int number;
     private String currentName;
     private String newName;
     private String seasonFolder;
+    private File file;
 
-    public FileToReplace(String currentName, String newName, String seasonFolder) {
+    public FileToReplace(int number, String currentName, String newName, String seasonFolder, File file) {
+        this.number = number;
         this.currentName = currentName;
         this.newName = newName;
         this.seasonFolder = seasonFolder;
+        this.file = file;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
     }
 
     public String getCurrentName() {
